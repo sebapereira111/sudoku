3f50014939288be023edd9a883882db5b424523c
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2025-08-21 14:44:09 -0300

    Actualizacion de comentarios en useTablero

--- src/hooks/useTablero.jsx
+++ src/hooks/useTablero.jsx
@@ -1,6 +1,10 @@
 import { tableroVacio } from "../constants/tableros";
 import { useState } from "react";
 
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// En este archivo se encuentran centralizadas las variables usadas para administrar el tablero
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
 function useTablero() {
     // Tablero resultado en base al cual se realiza el resto del juego
     const [tableroResultado, setTableroResultado] = useState(structuredClone(tableroVacio));
@@ -8,7 +12,8 @@ function useTablero() {
     const [tableroInicial, setTableroInicial] = useState(structuredClone(tableroVacio));
     // Tablero de trabajo, es el que se muestra en pantalla
     const [tableroActual, setTableroActual] = useState(structuredClone(tableroVacio));
-    // El box que esta selecionado actualmente en el tablero filaBloqueIndex = 3 es ninguno box seleccionado
+    // El box que esta selecionado actualmente en el tablero
+    // Si esta seleccionado filaBloqueIndex = 3 es ningun box seleccionado
     const [boxSeleccionado, setBoxSeleccionado] = useState({
         filaBloqueIndex: 3,
         colBloqueIndex: 0,
